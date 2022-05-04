a99162c2d8e512fd4efc2ced2efea6d9875f51ea
victorgmaciel <vvv.maciel@gmail.com>
2022-05-03 17:39:58 -0700

    Completed work for post and forum/blog page.

--- Better-Educators/src/pages/home.js
+++ Better-Educators/src/pages/home.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import '../Home.css'
+import logo from '../logo.png'
+import { db, auth } from '../firebase/firebase';
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
 
 
-const Home = () => {
 
-    return(
-        <div>
-            <h1>Home Page</h1>
-            <p>
-                Welcome to Better Educators
-            </p>
+function Home({ isAuth }) {
+    const [postLists, setPostList] = useState([]);
+    const postsCollectionRef = collection(db, "posts")
+
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollectionRef);
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+
+        getPosts();
+    });
+    return (
+        <div className='homePage'>
+            {postLists.map((post) => {
+                return <div className='post'>
+                    <div className='postHeader'>
+                        <div className='title'>
+                            <h1>{post.title}</h1>
+                        </div>
+                    </div>
+                    <div className='postTextContainer'>{post.postText}</div>
+                    <h4>@ {post.author.name}</h4>
+                </div>
+            })}
         </div>
     );
 }
--- Better-Educators/src/pages/post.js
+++ Better-Educators/src/pages/post.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import '../Post.css'
+import { addDoc, collection } from 'firebase/firestore'
+import { db, auth } from '../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
 
 
-const Post = () => {
+function Post({ isAuth }) {
+    const [title, setTitle] = useState('');
+    const [postText, setPostText] = useState('');
 
-    return(
-        <div>
-            <h1>Post</h1>
-            <p>
-                Here be Posts
-            </p>
+    const postsCollectionRef = collection(db, "posts")
+    let navigate = useNavigate();
+    
+    const createPost = async () => {
+        await addDoc(postsCollectionRef, {
+            title, postText, author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+        });
+        navigate('/home')
+    };
+    useEffect(() => {
+        if (!isAuth) {
+            navigate('/login');
+        }
+    }, []);
+
+    return (
+        <div className='createPostPage'>
+            <div className='cpContainer'>
+                <h1>Create a Post</h1>
+                <div className='inputGp'></div>
+                <label>title:</label>
+                <input placeholder="title.." onChange={(event) => { setTitle(event.target.value) }} />
+                <div className='inputGp'>
+                    <label>Post:</label>
+                    <textarea placeholder="Post.." onChange={(event) => { setPostText(event.target.value) }} />
+                </div>
+                <button onClick={createPost}>Submit</button>
+
+            </div>
         </div>
     );
 }
