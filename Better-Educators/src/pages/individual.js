0c4285f8bc56d4d84566b4f67c17f266929dff2c
Arjun Vaghela <Arjun@MacBook-Pro-6.local>
2022-05-16 10:59:02 -0700

    delete post button added with some functionality

--- Better-Educators/src/pages/individual.js
+++ Better-Educators/src/pages/individual.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 import '../Individual.css';
-import { doc, getDoc, collection } from 'firebase/firestore'
+import { doc, getDoc, collection, addDoc, deleteDoc } from 'firebase/firestore'
 import { db, auth, firestore } from '../firebase/firebase';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import Comments from './comments';
@@ -27,7 +27,6 @@ function IndividualPost({ isAuth }) {
         }
         getPost();
     }, []);
-
     if (isAuth && post) {
         return (
             <div className="individualPage">
@@ -36,6 +35,9 @@ function IndividualPost({ isAuth }) {
                         <button>
                             <Link to={-1}>Back</Link>
                         </button>
+                    </div>
+                    <div className="deleteButton">
+                        <DeletePost props={{ postId: postId }} />
                     </div>
                     <div className='ip-postHeader'>
                         <div className='ip-title'>
@@ -53,4 +55,24 @@ function IndividualPost({ isAuth }) {
         );
     }
 }
+
+function DeletePost(props) {
+    let navigate = useNavigate();
+    const postRef = doc(db, "posts", props.props.postId);
+    const postDelete = async () => {
+        await deleteDoc(postRef);
+        navigate(0); // This refreshes the page but there are issues:
+        /*
+            When it refreshes the page, it automatically logs the user out.
+            The comment deleting itself works, but because it logs the user out upon refreshing,
+            they won't be able to see the update until after they log in.
+            In other words, we need to make it so that upon refreshing, users will not get logged out
+            of their session.
+        */
+    }
+    return (
+        <button onClick={postDelete}>Delete Post</button>
+    )
+}
+
 export default IndividualPost;
